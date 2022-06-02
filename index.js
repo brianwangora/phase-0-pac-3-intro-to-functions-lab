104fead1a4a28616349665fe7c9b74536af43358
Brian Wangora <brianwangora@gmail.com>
2022-06-02 19:11:16 +0300

    commit message

---
+++ index.js
@@ -0,0 +1,30 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+function whisper(string) {
+    return string.toLowerCase();
+}
+function logShout(string) {
+    console.log(string.toUpperCase());
+}
+function logWhisper(string) {
+    console.log(string.toLowerCase());
+}
+
+
+
+function sayHiToHeadphonedRoommate(string) {
+    `string` === string.toLowerCase()
+        return "I can't hear you!"
+} 
+
+function sayHiToHeadphonedRoommate(string) {
+    `string` === string.toUpperCase()
+        return "YES INDEED!"
+}
+
+function sayHiToHeadphonedRoommate(string) {
+    `string` === "Let's have dinner together!"()
+        return "I would love to!"
+}    
+
